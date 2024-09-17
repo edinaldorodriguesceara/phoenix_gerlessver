60ed155d432c04d27c70416397d57e6be4169da0
Edinaldo Rodrigues <edinaldorodriguesceara@gmail.com>
2024-09-17 19:51:48 -0300

    Substituindo o hostname e porta do banco de dados por variáveis de ambiente

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,8 @@ import Config
 config :project, Project.Repo,
   username: System.get_env("DATABASE_USERNAME"),
   password: System.get_env("DATABASE_PASSWORD"),
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_HOST"),
+  port: System.get_env("DATABASE_PORT"),
   database: "project_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
